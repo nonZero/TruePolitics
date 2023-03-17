50e593f2d2b3c9b72a8e0280d99cf6e541415bd3
Udi Oron <udioron@gmail.com>
2023-03-17 14:33:50 +0200

    translate models to hebrew

--- statements/admin.py
+++ statements/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from statements import models
 
@@ -9,9 +10,11 @@ admin.site.register(models.Person)
 @admin.register(models.Statement)
 class StatementAdmin(admin.ModelAdmin):
     list_display = (
-        "__str__",
+        "person",
+        "content",
         "date",
         "get_topics",
+        "rating",
     )
     list_filter = ("topics",)
     date_hierarchy = "date"
@@ -19,3 +22,5 @@ class StatementAdmin(admin.ModelAdmin):
 
     def get_topics(self, instance):
         return ", ".join(str(t) for t in instance.topics.all())
+
+    get_topics.short_description = _("topics")
--- statements/models.py
+++ statements/models.py
@@ -3,19 +3,20 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    affiliation = models.CharField(max_length=500, blank=False)
+    name = models.CharField(_("name"), max_length=250, unique=True)
+    affiliation = models.CharField(_("affiliation"), max_length=500, blank=False)
 
     def __str__(self):
         return self.name
 
     class Meta:
+        verbose_name = _("person")
         verbose_name_plural = _("people")
 
 
 class Topic(models.Model):
-    title = models.CharField(max_length=250, unique=True)
-    description = models.TextField(blank=True)
+    title = models.CharField(_("title"), max_length=250, unique=True)
+    description = models.TextField(_("description"), blank=True)
 
     def __str__(self):
         return self.title
@@ -26,10 +27,18 @@ class Topic(models.Model):
 
 
 class Statement(models.Model):
-    person = models.ForeignKey(Person, models.PROTECT, related_name="statements")
-    topics = models.ManyToManyField(Topic, blank=True, related_name="statements")
-    date = models.DateField()
-    content = models.TextField()
+    person = models.ForeignKey(
+        Person, models.PROTECT, related_name="statements", verbose_name=_("person")
+    )
+    topics = models.ManyToManyField(
+        Topic, blank=True, related_name="statements", verbose_name=_("topics")
+    )
+    date = models.DateField(_("date"))
+    content = models.TextField(_("content"))
+
+    class Meta:
+        verbose_name = _("statement")
+        verbose_name_plural = _("statements")
 
     def __str__(self):
         return f'"{self.content}" by {self.person}'
@@ -39,8 +48,10 @@ class Statement(models.Model):
         ORANGE = 2, _("Half True")
         RED = 3, _("False")
 
-    rating = models.IntegerField(choices=Rating.choices, null=True)
-    review = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(
+        choices=Rating.choices, null=True, verbose_name=_("rating")
+    )
+    review = models.TextField(blank=True, null=True, verbose_name=_("review"))
 
 
 class Resource(models.Model):
