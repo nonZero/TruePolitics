b6c2c0407a6dcbbd9ca53f7ba23f62e617f70c8f
gnpashi <guynatan@gmail.com>
2023-03-17 17:26:48 +0200

    person sidebar

--- statements/urls.py
+++ statements/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("", views.StatementListView.as_view(), name="list"),
     path("s/<int:pk>/", views.StatementDetailView.as_view(), name="detail"),
     path("p/<int:pk>/", views.PersonDetailView.as_view(), name="person"),
-
+    path(
+        "p/<int:pk>/<int:topic_pk>/",
+        views.PersonTopicDetailView.as_view(),
+        name="person_topic",
+    ),
 ]
--- statements/views.py
+++ statements/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.utils.translation import gettext_lazy as _
 
@@ -22,3 +23,16 @@ class PersonDetailView(DetailView):
 
     def get_topics(self):
         return models.Topic.objects.filter(statements__person=self.object).distinct()
+
+    def get_statements(self):
+        return self.object.statements.all()
+
+
+class PersonTopicDetailView(PersonDetailView):
+    def get_statements(self):
+        return super().get_statements().filter(topics=self.topic)
+
+    def get(self, request, *args, **kwargs):
+        self.topic = get_object_or_404(models.Topic, pk=self.kwargs["topic_pk"])
+
+        return super().get(request, *args, **kwargs)
